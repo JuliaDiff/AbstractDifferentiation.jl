3e5056dee79dba218ac5bf1eebe4c61cdd8457b0
Seth Axen <seth.axen@gmail.com>
2022-01-22 23:34:51 +0100

    Create finitedifferences.jl

---
+++ src/finitedifferences.jl
@@ -0,0 +1,44 @@
+using .FiniteDifferences: FiniteDifferences
+
+"""
+    FiniteDifferencesBackend{M}
+
+AD backend that uses forward mode with FiniteDifferences.jl.
+
+The type parameter `M` is the type of the method used to perform finite differences.
+"""
+struct FiniteDifferencesBackend{M} <: AbstractFiniteDifference
+    method::M
+end
+
+"""
+    FiniteDifferencesBackend(method=FiniteDifferences.central_fdm(5, 1))
+
+Create an AD backend that uses forward mode with FiniteDifferences.jl.
+"""
+FiniteDifferencesBackend() = FiniteDifferencesBackend(FiniteDifferences.central_fdm(5, 1))
+
+@primitive function jacobian(ba::FiniteDifferencesBackend, f, xs...)
+    return FiniteDifferences.jacobian(ba.method, f, xs...)
+end
+
+function derivative(ba::FiniteDifferencesBackend, f, xs::Number...)
+    return pullback_function(ba, f, xs...)(true)
+end
+
+function gradient(ba::FiniteDifferencesBackend, f, xs...)
+    return FiniteDifferences.grad(ba.method, f, xs...)
+end
+
+function pushforward_function(ba::FiniteDifferencesBackend, f, xs...)
+    return function pushforward(vs)
+        ws = FiniteDifferences.jvp(ba.method, f, tuple.(xs, vs)...)
+        return length(xs) == 1 ? (ws,) : ws
+    end
+end
+
+function pullback_function(ba::FiniteDifferencesBackend, f, xs...)
+    function pullback(vs)
+        return FiniteDifferences.j′vp(ba.method, f, vs, xs...)
+    end
+end
