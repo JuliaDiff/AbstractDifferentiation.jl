8b204a0b5267193fd92c2ea1e4f55394459ca79c
Seth Axen <seth.axen@gmail.com>
2022-01-22 01:04:15 +0100

    Test also backend with user-specified chunk size

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -3,35 +3,40 @@ using Test
 using ForwardDiff
 
 @testset "ForwardDiffBackend" begin
-    forwarddiff_backend = AD.ForwardDiffBackend()
-    @testset "Derivative" begin
-        test_derivatives(forwarddiff_backend)
-    end
-    @testset "Gradient" begin
-        test_gradients(forwarddiff_backend)
-    end
-    @testset "Jacobian" begin
-        test_jacobians(forwarddiff_backend)
-    end
-    @testset "Hessian" begin
-        test_hessians(forwarddiff_backend)
-    end
-    @testset "jvp" begin
-        test_jvp(forwarddiff_backend; vaugmented=true)
-    end
-    @testset "j′vp" begin
-        test_j′vp(forwarddiff_backend)
-    end
-    @testset "Lazy Derivative" begin
-        test_lazy_derivatives(forwarddiff_backend)
-    end
-    @testset "Lazy Gradient" begin
-        test_lazy_gradients(forwarddiff_backend)
-    end
-    @testset "Lazy Jacobian" begin
-        test_lazy_jacobians(forwarddiff_backend; vaugmented=true)
-    end
-    @testset "Lazy Hessian" begin
-        test_lazy_hessians(forwarddiff_backend)
+    backends = [
+        @inferred(AD.ForwardDiffBackend())
+        @inferred(AD.ForwardDiffBackend(; chunksize=Val{1}()))
+    ]
+    @testset for backend in backends
+        @testset "Derivative" begin
+            test_derivatives(backend)
+        end
+        @testset "Gradient" begin
+            test_gradients(backend)
+        end
+        @testset "Jacobian" begin
+            test_jacobians(backend)
+        end
+        @testset "Hessian" begin
+            test_hessians(backend)
+        end
+        @testset "jvp" begin
+            test_jvp(backend; vaugmented=true)
+        end
+        @testset "j′vp" begin
+            test_j′vp(backend)
+        end
+        @testset "Lazy Derivative" begin
+            test_lazy_derivatives(backend)
+        end
+        @testset "Lazy Gradient" begin
+            test_lazy_gradients(backend)
+        end
+        @testset "Lazy Jacobian" begin
+            test_lazy_jacobians(backend; vaugmented=true)
+        end
+        @testset "Lazy Hessian" begin
+            test_lazy_hessians(backend)
+        end
     end
 end
