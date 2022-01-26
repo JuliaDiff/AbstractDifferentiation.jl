00ed3f0483c723331d2fecfa243d7cb9288e5a16
Seth Axen <seth.axen@gmail.com>
2022-01-27 00:45:47 +0100

    Add Tracker backend

---
+++ src/tracker.jl
@@ -0,0 +1,25 @@
+using .Tracker: Tracker
+
+primal_value(x::Tracker.TrackedArray) = Tracker.data(x)
+primal_value(x::Tracker.TrackedReal) = Tracker.data(x)
+primal_value(x::AbstractArray{<:Tracker.TrackedReal}) = Tracker.data.(x)
+
+"""
+    TrackerBackend
+
+AD backend that uses reverse mode with Tracker.jl.
+"""
+struct TrackerBackend <: AbstractReverseMode end
+
+@primitive function pullback_function(ba::TrackerBackend, f, xs...)
+    value, back = Tracker.forward(f, xs...)
+    function pullback(ws)
+        if ws isa Tuple && !(value isa Tuple) 
+            @assert length(ws) == 1
+            map(Tracker.data, back(ws[1]))
+        else
+            map(Tracker.data, back(ws))
+        end
+    end
+    return pullback
+end
