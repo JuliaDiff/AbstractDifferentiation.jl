fb349179f33bad7393aa1787288292da343749ba
David Widmann <devmotion@users.noreply.github.com>
2022-02-09 21:55:51 +0100

    Fix CRC imports

--- src/ruleconfig.jl
+++ src/ruleconfig.jl
@@ -3,12 +3,12 @@
 
 AD backend that uses reverse mode with any ChainRules-compatible reverse-mode AD package.
 """
-struct ReverseRuleConfigBackend{RC<:RuleConfig{>:ChainRulesCore.HasReverseMode}} <: AbstractReverseMode
+struct ReverseRuleConfigBackend{RC<:ChainRulesCore.RuleConfig{>:ChainRulesCore.HasReverseMode}} <: AbstractReverseMode
     ruleconfig::RC
 end
 
 AD.@primitive function pullback_function(ab::ReverseRuleConfigBackend, f, xs...)
-    _, back = rrule_via_ad(ab.ruleconfig, f, xs...)
+    _, back = ChainRulesCore.rrule_via_ad(ab.ruleconfig, f, xs...)
     pullback(vs) = Base.tail(back(vs))
     pullback(vs::Tuple{Any}) = Base.tail(back(first(vs))
     return pullback
