3218c0866d027fe795a84a20b51759eeeb6770ed
David Widmann <devmotion@users.noreply.github.com>
2023-03-03 21:53:57 +0100

    New Zygote context in every call to `AD.pullback_function` (#77)
    * New Zygote context in every call to `AD.pullback_function`
    
    * Make fix more modular

--- ext/AbstractDifferentiationZygoteExt.jl
+++ ext/AbstractDifferentiationZygoteExt.jl
@@ -9,4 +9,9 @@ end
 
 AD.ZygoteBackend() = AD.ReverseRuleConfigBackend(Zygote.ZygoteRuleConfig())
 
+# Context should not persist between different AD calls: fixes #69
+function AD.ruleconfig(::AD.ReverseRuleConfigBackend{<:Zygote.ZygoteRuleConfig})
+    return Zygote.ZygoteRuleConfig()
+end
+
 end # module
--- test/ruleconfig.jl
+++ test/ruleconfig.jl
@@ -30,4 +30,26 @@ using Zygote
             test_lazy_jacobians(backend)
         end
     end
+
+    # issue #69
+    @testset "Zygote context" begin
+        ad = AD.ZygoteBackend()
+
+        # example in #69: context is not mutated
+        @test ad.ruleconfig.context.cache === nothing
+        @test AD.derivative(ad, exp, 1.0) === (exp(1.0),)
+        @test ad.ruleconfig.context.cache === nothing
+        @test AD.derivative(ad, exp, 1.0) === (exp(1.0),)
+        @test ad.ruleconfig.context.cache === nothing
+
+        # Jacobian computation still works
+        # https://github.com/JuliaDiff/AbstractDifferentiation.jl/pull/70#issuecomment-1449481724
+        function f(x, a)
+           r = Ref(x)
+           r[] = r[] + r[]
+           r[] = r[] * a
+           r[]
+        end
+        @test AD.jacobian(ad, f, [1, 2, 3], 3) == ([6.0 0.0 0.0; 0.0 6.0 0.0; 0.0 0.0 6.0], [2.0, 4.0, 6.0])
+    end
 end
