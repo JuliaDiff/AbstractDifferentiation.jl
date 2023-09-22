4677ce170fda01f788d14f3c62dbecb13154ef45
David Widmann <devmotion@users.noreply.github.com>
2023-09-22 15:12:23 +0200

    Reformat existing code

--- ext/AbstractDifferentiationChainRulesCoreExt.jl
+++ ext/AbstractDifferentiationChainRulesCoreExt.jl
@@ -3,7 +3,9 @@ module AbstractDifferentiationChainRulesCoreExt
 import AbstractDifferentiation as AD
 using ChainRulesCore: ChainRulesCore
 
-AD.@primitive function value_and_pullback_function(ba::AD.ReverseRuleConfigBackend, f, xs...)
+AD.@primitive function value_and_pullback_function(
+    ba::AD.ReverseRuleConfigBackend, f, xs...
+)
     value, back = ChainRulesCore.rrule_via_ad(AD.ruleconfig(ba), f, xs...)
     function rrule_pullback(vs)
         _vs = if vs isa Tuple && !(value isa Tuple)
